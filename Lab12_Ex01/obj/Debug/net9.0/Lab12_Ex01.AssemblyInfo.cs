be1b0544003ec5d9e6060f8f3e47dc1a4190a64c
natthanitcha687 <babynitcha0@gmail.com>
2025-02-23 22:46:21 +0700

    update1

--- Lab12_Ex01/obj/Debug/net9.0/Lab12_Ex01.AssemblyInfo.cs
+++ Lab12_Ex01/obj/Debug/net9.0/Lab12_Ex01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab12_Ex01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f45a0b27d597da273c6bdfd62d490b8719d2242")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+095930b5f92db59ca2b7f998689cb0d075afd12b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab12_Ex01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab12_Ex01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
