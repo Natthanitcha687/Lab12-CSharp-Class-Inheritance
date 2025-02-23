853fa25221e863e772e9580a46b83c8dc73c353f
natthanitcha687 <babynitcha0@gmail.com>
2025-02-23 21:18:58 +0700

    update2

---
+++ Lab12_Ex02/Program.cs
@@ -0,0 +1,23 @@
+﻿var dc = new DerivedClass();
+dc.Method1(dc.Field1);
+dc.Method2(dc.Field1);
+dc.Method1(dc.Field2);
+dc.Method2(dc.Field2);
+
+class BaseClass
+{
+    public string Field1 = "Field 1 in BaseClass";
+    public void Method1(string value)
+    {
+        System.Console.WriteLine($"Method1() in BaseClass, string input = {value} ");
+    }
+}
+class DerivedClass: BaseClass
+{
+    public string Field2 = "Field 2 in DerivedClass";
+    public void Method2(string value)
+    {
+        System.Console.WriteLine($"Method2() in DerivedClass, string input = {value} ");
+    }
+
+}
